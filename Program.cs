c442144115ec9e2a42b4ab54e2cf0f19f4edf427
Victor Ramon Balchaki <95146779+Balchaki@users.noreply.github.com>
2024-08-17 15:56:08 -0300

    Solucao desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Smartphones");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("123456789", "Iphone 12", "123456789", 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Github");
+Console.WriteLine();
+
+Smartphone nokia = new Nokia("987654321", "Nokia 3310", "987654321", 32);
+Console.WriteLine("Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+Console.WriteLine();
